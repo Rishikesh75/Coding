693f473e135aa1869e86e043db59c579fe578ad6
Kotla Rishikesh Reddy <kotlarishikesh_reddy@trimble.com>
2025-07-17 22:12:31 +0530

    Generic Collection has been Practised

--- Basics/obj/Debug/net9.0/Basics.AssemblyInfo.cs
+++ Basics/obj/Debug/net9.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb58d5d78d653c4a69c556f061a188078885098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb65ee616aba3253fcd387c8c84ea494292d9f89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
