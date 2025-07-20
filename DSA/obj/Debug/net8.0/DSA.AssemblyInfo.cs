a0d86d66e6eec4833a93366193c42aba80a44d77
Kotla Rishikesh Reddy <kotlarishikesh_reddy@trimble.com>
2025-07-20 21:39:19 +0530

    DSA in csharp

--- DSA/obj/Debug/net8.0/DSA.AssemblyInfo.cs
+++ DSA/obj/Debug/net8.0/DSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb65ee616aba3253fcd387c8c84ea494292d9f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693f473e135aa1869e86e043db59c579fe578ad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
