0e8295051b741e9cc59cac6cb1fd82b50bdc90db
Rishikesh75 <kotlarishikesh27@gmail.com>
2025-11-12 17:09:46 +0530

    Clearing the Ith Bit

--- BitManupulations/5.cpp
+++ BitManupulations/5.cpp
@@ -1,7 +1,19 @@
+// Clear ith Bit
 #include<iostream>
 using namespace std;
+void clearithbit(int& num,int i)
+{
+    num = num & ~(1 << i);
+}
 int main()
 {
-
+    cout<<"Enter the Number:"<<endl;
+    int num;
+    cin>>num;
+    cout<<"Enter the ith Bit:"<<endl;
+    int i;
+    cin>>i;
+    clearithbit(num,i);
+    cout<<"After Clearning the ith Bit:"<<num<<endl;
     return 0;
 }
