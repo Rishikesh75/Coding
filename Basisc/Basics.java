bd047d7355a93250a0abe2ca6cc71835c6c9e2de
Rishikesh75 <kotlarishikesh27@gmail.com>
2025-12-29 22:59:23 +0530

    Learning Java

--- Basisc/Basics.java
+++ Basisc/Basics.java
@@ -1,7 +1,7 @@
 public class Basics
 {
-    public static void main(String[] args) {
-        //System.out.println("HelloWord");
+    public static void main(String[] args)
+    {
+        System.out.println("HelloWorld");
     }
-
 }
