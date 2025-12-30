b779c4dbab9e2b71c1f00e11b6369a6b140f4481
Rishikesh75 <kotlarishikesh27@gmail.com>
2025-12-30 11:04:51 +0530

    Learning Basics in Java

--- Basisc/Basics.java
+++ Basisc/Basics.java
@@ -1,7 +1,29 @@
+import java.util.*;
 public class Basics
 {
+
+    public static void HelloWorld()
+    {
+        System.out.println("Hello World");
+    }
     public static void main(String[] args)
     {
         System.out.println("HelloWorld");
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        switch(val%2)
+        {
+            case 0:
+                System.out.println("EVEN NUMBER");
+                break;
+            case 1:
+                System.out.println("ODD NUMBER");
+                break;
+        }
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("Rishi");
+        }
+        HelloWorld();
     }
 }
