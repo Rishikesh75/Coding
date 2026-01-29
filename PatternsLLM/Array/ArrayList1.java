00e50af4f983d38b63500a6386dd4366fcc8f336
Rishikesh75 <kotlarishikesh27@gmail.com>
2026-01-30 02:45:53 +0530

    ArrayList phase1 Done

--- PatternsLLM/Array/ArrayList1.java
+++ PatternsLLM/Array/ArrayList1.java
@@ -1,158 +1,207 @@
+
 import java.util.*;
-public class ArrayList1
+public class Main
 {
     public static Scanner sc = new Scanner(System.in);
-    public static List<Integer> createArr(int n)
+    public static void CreateArr(List<Integer>list)
     {
-        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter the size of the Arr:");
+        int n = sc.nextInt();
+        // list.resize(n);
         for(int i=0;i<n;i++)
         {
-            System.out.println("Enter the number:");
-            int temp = sc.nextInt();
-            list.add(temp);
+            int number = sc.nextInt();
+            list.add(number);
         }
-        return list;
     }
-    public static void display(List<Integer> list)
+    public static void display(List<Integer>list)
     {
-        System.out.print("Arr:");
-        for(var element : list)
-        {
-            System.out.printf("%d\t",element);
-        }
-        System.out.println();
-        return;
-    }
-    public static int findMax(List<Integer>list)
-    {
-        int val = Integer.MIN_VALUE;
         for(var num : list)
         {
-            val = Integer.max(val,num);
+            System.out.printf("%d\t",num);
         }
-        return val;
+        System.out.println();
     }
-    public static int findMin(List<Integer>list)
+    public static int findLargest(List<Integer>list)
     {
-        int val = Integer.MAX_VALUE;
+        int val = Integer.MIN_VALUE;
         for(int i=0;i<list.size();i++)
         {
-            val = Integer.min(list.get(i),val);
+            val = Math.max(val,list.get(i));
         }
         return val;
-    }
-    public static int findSum(List<Integer>list)
-    {
-        int val = 0;
-        var iterator = list.iterator();
-        while(iterator.hasNext())
-        {
-            val = val + iterator.next();
-        }
-        return val;
-    }
-    public static List<Integer> findEvenAndOddCount(List<Integer>arr)
-    {
-        List<Integer>ans = new ArrayList<>();
-        Integer evenCount = 0;
-        Integer oddCount = 0;
-        for(var num : arr)
-        {
-                if(num%2 == 0)
-                {
-                    evenCount += 1;
-                }
-                else
-                {
-                    oddCount += 1;
-                }
-        }
-        ans.add(evenCount);
-        ans.add(oddCount);
-        return ans;
-    }
-    public static void reverseArray(List<Integer>arr)
-    {
-        int i = 0;
-        int j = arr.size()-1;
-        while(i<j)
-        {
-            int v = arr.get(i);
-            arr.set(i,arr.get(j));
-            arr.set(j,v);
-            i = i+1;
-            j = j-1;
-        }
-        display(arr);
     }
     public static int findSecondLargest(List<Integer>list)
     {
-        int firstLargest = list.get(0) > list.get(1) ? list.get(0) : list.get(1);
-        int secondLargest  = firstLargest == list.get(0) ? list.get(1) : list.get(0);
-        for(int i=2;i<list.size();i++)
+        int firstMax = list.get(0);
+        int secondMax = Integer.MIN_VALUE;
+        for(int i=1;i<list.size();i++)
         {
-            if(list.get(i)>firstLargest)
+            if(list.get(i) > firstMax)
             {
-                secondLargest = firstLargest;
-                firstLargest = list.get(i);
+                secondMax = firstMax;
+                firstMax = list.get(i);
             }
-            else if(list.get(i)>secondLargest)
+            else if(list.get(i) > secondMax)
             {
-                secondLargest = list.get(i);
+                secondMax = list.get(i);
             }
         }
-        return secondLargest;
+        return secondMax;
     }
-    public static boolean checkSorted(List<Integer>list)
+    public static boolean checkArraySorted(List<Integer>list)
     {
         for(int i=1;i<list.size();i++)
         {
-            if(list.get(i)>list.get(i-1))
+            if(list.get(i)<list.get(i-1))
             {
                 return false;
             }
         }
         return true;
     }
-
-    public static boolean linearSearch(List<Integer>list,int val)
+    public static Mypair countEvenOdd(List<Integer>list)
     {
-        for(var num : list)
+        Mypair pair = new Mypair();
+        for(int i=0;i<list.size();i++)
         {
-            if(num == val) return true;
+            if(list.get(i)%2 == 0)
+            {   
+                pair.evenNumberCount += 1;
+            }
+            else 
+            {
+                pair.oddNumberCount += 1;
+            }
         }
-        return false;
+        return pair;
     }
-    public static List<Integer> removeDuplicates(List<Integer> list) {
-        Set<Integer> set = new LinkedHashSet<>();
-        for (int num : list) {
-            set.add(num);
+    public static void reverseArray(List<Integer>list)
+    {
+        int start = 0;
+        int end = list.size()-1;
+        while(start<end)
+        {
+            int temp = list.get(start);
+            list.set(start,list.get(end));
+            list.set(end,temp);
+            start=start+1;
+            end=end-1;
         }
-        return new ArrayList<>(set);
+        
+    }
+    public static void removeDuplicate(List<Integer>list)
+    {
+        Set<Integer>set = new HashSet<>(list);
+        
+        Iterator<Integer> it = set.iterator();
+        while(it.hasNext())
+        {
+            Integer val = it.next();
+            System.out.printf("%d\t",val);
+            it.remove();
+        }
+        System.out.printf("size:%d",set.size());
+    }
+    public static boolean isPalindrome(List<Integer>list)
+    {
+        int i = 0;
+        int j = list.size()-1;
+        while(i<j)
+        {
+            if(list.get(i) != list.get(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static int missingNumber(List<Integer>list)
+    {
+        int val = 0;
+        for(int i=0;i<list.size();i++)
+        {
+            val = val ^ list.get(i) ^ i;
+        }
+        val = val ^ list.size();
+        return val;
+    }
+    public static void moveZerosEnd(List<Integer>list)
+    {
+        int start = 0;
+        int end = list.size()-1;
+        while(start<end)
+        {
+            if(list.get(start) != 0)
+            {
+                start = start + 1;
+                continue;
+            }
+            if(list.get(end) == 0)
+            {
+                end = end - 1;
+                continue;
+            }
+            
+            int temp = list.get(start);
+            list.set(start,list.get(end));
+            list.set(end,temp);
+            start = start + 1;
+            end = end - 1;
+        }
+    }
+    public static void frequencyCount(List<Integer>list)
+    {
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int num :list)
+        {
+            if(map.containsKey(num))
+            {
+                map.replace(num,map.get(num)+1);
+                continue;
+            }
+            map.put(num,1);
+        }
+        
+        for(Map.Entry<Integer,Integer> item : map.entrySet())
+        {
+            System.out.printf("%d %d",item.getKey(),item.getValue());
+        }
     }
     public static void main(String[] args)
     {
-
-
-        Integer n;
-        System.out.println("Enter the Size of the ArrayList:");
-        n = sc.nextInt();
-
-        List<Integer> list = createArr(n);
-        display(list);
-        System.out.printf("Max Value:%d\n",findMax(list));
-        System.out.printf("Min Value: %d\n",findMin(list));
-        System.out.printf("Sum value:%d\n",findSum(list));
-
-        var ans = findEvenAndOddCount(list);
-        System.out.printf("even count :%d odd count :%d\n",ans.get(0),ans.get(1));
-
-        reverseArray(list);
-
-        System.out.printf("SecondLargest Element: %d\n",findSecondLargest(list));
-
-        System.out.printf("Check Sorted%b\n",checkSorted(list));
-
-        System.out.printf("Element Found %b\n",linearSearch(list,1));
+        List<Integer> list = new ArrayList<>();
+        CreateArr(list);
+        // display(list);
+        // System.out.printf("Max Value:%d\n",findLargest(list));
+        // System.out.printf("Second Max Value:%d\n",findSecondLargest(list));
+        // System.out.printf("Sorted Array: %b\n",checkArraySorted(list));
+        // var ans = countEvenOdd(list);
+        // System.out.printf("Even Count Number:%d Odd Count Number:%d",ans.evenNumberCount,ans.oddNumberCount);
+        // reverseArray(list);
+        // System.out.println("Reversed Array:\n");
+        // display(list);
+        // System.out.println("Remove Duplicates Array:\n");
+        // removeDuplicate(list);
+        // System.out.printf("isPalindrome:%b",isPalindrome(list));
+        // System.out.printf("%d",missingNumber(list));
+        // moveZerosEnd(list);
+        // display(list);
+        // frequencyCount(list);
+        
+    }
+    
+    public static class Mypair
+    {
+        Integer evenNumberCount;
+        Integer oddNumberCount;
+        
+        Mypair()
+        {
+            this.evenNumberCount = 0;
+            this.oddNumberCount = 0;
+        }
     }
 }
