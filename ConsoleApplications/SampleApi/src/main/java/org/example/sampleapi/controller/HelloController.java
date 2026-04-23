ba331d37f90f4b3589530acb0a9fcee44c7eac55
Rishikesh75 <kotlarishikesh27@gmail.com>
2026-04-23 16:09:38 +0530

    Creating the ConsoleApplication and SimpleApi for Pratices in the VisualStudio

---
+++ ConsoleApplications/SampleApi/src/main/java/org/example/sampleapi/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.example.sampleapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "Spring Boot is Working";
+    }
+}
